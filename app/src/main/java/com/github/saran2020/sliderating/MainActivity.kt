cc52e836e15bcc9b8c2b7a442783d79148fbf605
Saran <saran.sankaran@gmail.com>
2019-04-21 04:25:47 +0530

    Made the Main activity to use callback to set the values

--- app/src/main/java/com/github/saran2020/sliderating/MainActivity.kt
+++ app/src/main/java/com/github/saran2020/sliderating/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.github.saran2020.sliderating
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ratingView = findViewById<SlideRatingView>(R.id.slide_rating)
+        val ratingTextView = findViewById<TextView>(R.id.rating_text)
+
+        ratingView.callback = object : SlideRatingView.RatingChangeCallback {
+            override fun onRatingChanged(previous: Float, new: Float) {
+                ratingTextView.text = "$new"
+            }
+        }
     }
 }
