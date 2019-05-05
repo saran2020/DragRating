48f3e50427d66beb109281ead19aedba2ad05b61
Saran <saran.sankaran@gmail.com>
2019-05-05 17:26:01 +0530

    Add test function for read and set rating to the app module

--- app/src/main/java/com/github/saran2020/app/MainActivity.kt
+++ app/src/main/java/com/github/saran2020/app/MainActivity.kt
@@ -2,6 +2,7 @@ package com.github.saran2020.app
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.TextView
 import com.github.saran2020.sliderating.SlideRatingView
 
@@ -22,5 +23,16 @@ class MainActivity : AppCompatActivity() {
                 ratingTextView?.text = "$new"
             }
         }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val rating = ratingView?.getRating() ?: 0
+        Log.d("buggy_bug", "current rating is $rating")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        ratingView?.setRating(4f)
     }
 }
